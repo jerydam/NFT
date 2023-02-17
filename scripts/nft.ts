fd3fe3ad61b3056b4927cd6d4f6a7e45d3716e20
Jerydam <jerydan148@gmail.com>
2023-02-17 04:12:37 +0100

    this nft

--- scripts/nft.ts
+++ scripts/nft.ts
@@ -1,14 +1,22 @@
 import { ethers } from "hardhat";
 
 async function main() {
-    const nftContract = await ethers.getContractFactory("NFTee");
+    const nftContract = await ethers.getContractFactory("MyNft");
+
+    const [owner, addr1, addr2] = await ethers.getSigners();
     
     const deployedNFTContract = await nftContract.deploy();
-  
+    
+    const IPFS = "QmWSQyZgSaj64tq5yYKP8ccUEtXuKowz9RySAtECgKyE1X";
     await deployedNFTContract.deployed();
   
     console.log("NFT Contract Address:", deployedNFTContract.address);
+
+    const MyMint = await deployedNFTContract.safeMint(owner.address,IPFS);
+    console.log(MyMint)
 }
+
+
 
 
 main().catch((error) => {
